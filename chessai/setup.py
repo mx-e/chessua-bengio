9117d21cf60349f3cfc764c1eb3e24273c8dbb9b
pupuis <luismueller2309@gmail.com>
2022-04-23 14:05:20 +0200

    Adds auto-search for highest c++ standard

--- chessai/setup.py
+++ chessai/setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup 
 from glob import glob 
-from pybind11.setup_helpers import Pybind11Extension
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 setup(
     ext_modules=[
         Pybind11Extension("chessai", sorted(glob("src/*.cpp")))
     ],
-    name="chessai"
+    name="chessai",
+    cmdclass={"build_ext": build_ext}
 )
