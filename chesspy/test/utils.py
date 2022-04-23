c8f3d49900f4f2e5823632e34ae20506b40169b8
mx-e <maxi.eissler@gmail.com>
2022-04-23 20:34:40 +0200

    fixing build and test workflow

--- chesspy/test/utils.py
+++ chesspy/test/utils.py
@@ -7,4 +7,6 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(get_char_to_int_tile_state(22), 0)
         self.assertEqual(get_char_to_int_tile_state("g"), 0)
         self.assertEqual(get_char_to_int_tile_state('p'), -1)
-        
+    
+    def test_fail_test(self):
+        self.assertAlmostEqual(1, 5)
