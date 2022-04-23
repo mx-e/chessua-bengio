7bdd7fd4fdf2966336ffaaa95babbc79100b8fc5
pupuis <luismueller2309@gmail.com>
2022-04-23 16:23:34 +0200

    Adds two c++ functions to demonstrate 'roundtrip' board state

--- chesscpp/src/pybind11_wrapper.cpp
+++ chesscpp/src/pybind11_wrapper.cpp
@@ -1,7 +1,10 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include "main.hpp"
 
 PYBIND11_MODULE(chesscpp, m) {
     m.doc() = "Chess-AI C++ Backend";
     m.def("BIG_LOOP", &big_loop, "Runs a big loop (for benchmarking/stat-checking)");
+    m.def("get_board", &get_board, "lala");
+    m.def("add_1_to_board", &add_1_to_board, "lala");
 }
