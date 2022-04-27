df9459a0fc0e4a8d0a327f62c56be776d6629ac2
pupuis <luismueller2309@gmail.com>
2022-04-27 13:51:57 +0200

    Fixes action failure from task and archives older cpp files

--- tasks.py
+++ tasks.py
@@ -4,7 +4,7 @@ from invoke import task
 def build(c, python='python'):
 
     print("\033[0;92m[PRE-BUILD]: test chesscpp")
-    c.run("cd chesscpp && invoke test", pty=True)
+    c.run("cd chesscpp && invoke test --remake", pty=True)
 
     print("\033[0;92m[BUILD]: Chess-AI module\033[0;37m")
     c.run(f"{python} -m pip install .", pty=True)
