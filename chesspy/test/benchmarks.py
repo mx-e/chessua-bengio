c20555d8d6a41736150ace11bad6da3c1cff3f72
mx-e <maxi.eissler@gmail.com>
2022-05-08 23:00:52 +0200

    adds more expressive benchmark results

--- chesspy/test/benchmarks.py
+++ chesspy/test/benchmarks.py
@@ -1,6 +1,6 @@
 from tkinter import N
 import unittest, logging, sys, pathlib, json, numpy as np, time
-from chesscpp import runtime_benchmark
+from chesscpp import runtime_benchmark, generate_moves
 from chesspy.game import import_fen, export_fen
 
 
@@ -16,24 +16,38 @@ class TestBenchmarks(unittest.TestCase):
                 f"Running move generation benchmark '{key}', with {len(states)} configurations, {n_runs} runs each..."
             )
             times = []
+            n_moves = []
             for state in states:
                 board = import_fen(state)
                 start = time.time()
 
                 enpassant = [tuple(board.en_passant_tile)] if not (board.en_passant_tile[0] == -1 and board.en_passant_tile[1] == -1) else []
                 runtime_benchmark(board.board_state, board.to_move, enpassant, *board.can_castle, board.n_reversible_halfmoves, board.n_moves, n_runs)
-            
-                for _ in range(n_runs):
-                    export_fen(board)
-                ##
                 end = time.time()
                 times.append(end - start)
 
+                uci_moves = generate_moves(board.board_state, board.to_move, enpassant, *board.can_castle, board.n_reversible_halfmoves, board.n_moves)
+                n_moves.append(len(uci_moves))
+
             mean_time_million_runs = np.array(times).mean() * (1e6 / n_runs)
+            mean_time_single_run_ms = np.array(times).mean() / n_runs * 1000
             log.debug(
-                f"Average speed is {mean_time_million_runs:.4f}s/million boards."
+                f"Speed Statistics:"
             )
-        
+            log.debug(
+                f"{mean_time_million_runs:.4f}s per million boards."
+            )
+            log.debug(
+                f"{mean_time_single_run_ms:.6f}ms per positions"
+            )
+            configs_per_s = n_runs / np.array(times).mean()
+            log.debug(
+                f"{configs_per_s} boards per second."
+            )
+            moves_per_s = n_runs / (np.array(times) / np.array(n_moves)).mean()
+            log.debug(
+                f"{moves_per_s} moves per second."
+            )
 
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stderr)
