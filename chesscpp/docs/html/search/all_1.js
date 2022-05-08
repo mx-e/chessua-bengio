cc0689593086aeb33b7d835a9b3b33ab97be4d2d
pupuis <luismueller2309@gmail.com>
2022-05-08 12:34:54 +0200

    Fix in UML diagrams

--- chesscpp/docs/html/search/all_1.js
+++ chesscpp/docs/html/search/all_1.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['castle_0',['Castle',['../class_castle.html',1,'']]],
-  ['castletransitmove_1',['CastleTransitMove',['../class_castle_transit_move.html',1,'']]],
-  ['castlingrights_2',['CastlingRights',['../struct_castling_rights.html',1,'']]],
-  ['colorcastlingrights_3',['ColorCastlingRights',['../struct_color_castling_rights.html',1,'']]]
+  ['castlingrights_1',['CastlingRights',['../struct_castling_rights.html',1,'']]],
+  ['colorcastlingrights_2',['ColorCastlingRights',['../struct_color_castling_rights.html',1,'']]]
 ];
