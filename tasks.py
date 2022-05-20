2410069a2006ce5206533fab15b218b2b943a3e3
mx-e <maxi.eissler@gmail.com>
2022-05-20 17:17:20 +0200

    fixing build script errors

--- tasks.py
+++ tasks.py
@@ -4,10 +4,9 @@ from invoke import task
 def build(c, python='python'):
 
     print("\033[0;92m[PRE-BUILD]: test chesscpp")
-    c.run("cd chesscpp && invoke test", pty=True)
+    c.run("cd chesscpp && invoke test --remake", pty=True)
 
     print("\033[0;92m[BUILD]: Chess-AI module\033[0;37m")
-    c.run(f"rm -r build", pty=True)
     c.run(f"{python} -m pip install . --force-reinstall", pty=True)
     
     print("\033[0;92m[POST-BUILD]: discover python tests\033[0;37m")
