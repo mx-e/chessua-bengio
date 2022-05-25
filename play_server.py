8890f7aad4d85574f61c335c88e58c818efbcf79
Valentin Obermeier <code@futureweb-berlin.de>
2022-05-25 17:22:03 +0200

    Adapt play_server.py for bestmove

--- play_server.py
+++ play_server.py
@@ -3,7 +3,7 @@ import socketio
 import asyncio
 import random
 
-from chesscpp import generate_moves
+from chesscpp import runtime_benchmark_bestmove
 
 from chesspy.game import import_fen
 
@@ -26,11 +26,17 @@ async def on_color(data):
 @sio.on('board_state')
 async def board_state(data):
     board = import_fen(data)
+    if " w " in data:
+        current_color = "white"
+    else:
+        current_color = "black"
+    if current_color != player_color and player_color != '':
+        return
+    print("received board, starting ai")
     enpassant = [tuple(board.en_passant_tile)] if not (board.en_passant_tile[0] == -1 and board.en_passant_tile[1] == -1) else []
-    uci_moves = generate_moves(board.board_state, board.to_move, enpassant, *board.can_castle, board.n_reversible_halfmoves, board.n_moves)
+    move = runtime_benchmark_bestmove(False, board.board_state, board.to_move, 3, enpassant, *board.can_castle, board.n_reversible_halfmoves, board.n_moves)
     print('board state' + data)
-    move = random.choice(uci_moves)
-    print('picked random move: '+ move)
+    print('picked best move: '+ move)
     await sio.emit('move', move)
 
 
