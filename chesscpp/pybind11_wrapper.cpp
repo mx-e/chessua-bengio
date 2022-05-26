5379d9411ea3b4a73c86e866a396b69462ad1573
pupuis <luismueller2309@gmail.com>
2022-05-26 21:49:33 +0200

    Integrates new version with pybind

--- chesscpp/pybind11_wrapper.cpp
+++ chesscpp/pybind11_wrapper.cpp
@@ -8,7 +8,5 @@ PYBIND11_MODULE(chesscpp, m)
 {
     m.doc() = "Chess-AI C++ Backend";
     m.def("generate_moves", &generate_moves, "Generates all possible moves given an initial state.");
-    m.def("runtime_benchmark", &runtime_benchmark, "");
-    m.def("bb_generate_moves", &bb_generate_moves, "Generates all possible moves given a state using bitboards.");
-    m.def("bb_runtime_benchmark", &bb_runtime_benchmark, "Runs runtime benchmark for bitboard move generation n times.");
+    m.def("runtime_benchmark", &runtime_benchmark, "Runs runtime benchmark for bitboard move generation n times.");
 }
