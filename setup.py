35ec29079bb6ba072fa667913542e3be436cd144
pupuis <luismueller2309@gmail.com>
2022-07-05 19:05:08 +0200

    Basic setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 class BuildExt(build_ext):
     def build_extension(self, ext) -> None:
-        ext.extra_compile_args = ['-g', '-o3']
+        ext.extra_compile_args = ['-g', '-std=c++20', '-D']
         return super().build_extension(ext)
 
 setup(
@@ -12,6 +12,6 @@ setup(
         Pybind11Extension("chesscpp", sorted(["./chesscpp/pybind11_wrapper.cpp"] + glob("./chesscpp/src/*.cpp")))
     ],
     name="chess-ai",
-    cmdclass={"build_ext": build_ext},
+    cmdclass={"build_ext": BuildExt},
     packages=['chesspy']
 )
